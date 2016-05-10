ed3ba98513ec5194213ae65d0ee0ceff7320b464
ahirschberg <alexhirschberg97@gmail.com>
2016-05-10 19:38:17 -0400

    Got proof of concept for HashLocationStrategy routing working. Had to dig through the source files to figure out where it was though, since it's not referenced in the docs!

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,23 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
 import 'logger_service.dart';
 import 'heroes/heroes_component.dart';
 import 'package:angular2_getting_started/heroes/hero_service_provider.dart';
 
+@RouteConfig(const [
+  const Route(
+    path: '/heroes/',
+    component: HeroesComponent,
+    useAsDefault: true
+  )
+])
+
 @Component(
     selector: 'my-app',
-    template: '<my-heroes></my-heroes>',
+    template: '<router-outlet></router-outlet>',
 //    encapsulation: ViewEncapsulation.Native, // uncomment for native shadow dom on chrome!
     providers: const [Logger, heroServiceProvider],
-    directives: const [HeroesComponent]
+    directives: const [HeroesComponent, ROUTER_DIRECTIVES]
 )
 class AppComponent {}
